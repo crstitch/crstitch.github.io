f5b2aff84845b286b618e743b1b42ddcae1f4adc
crstitch <lktoshev@gmail.com>
2022-07-31 12:52:40 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.header').height($(window).height());
-    })
+})
+
+$(".navbar a").click(function() {
+    $("body,html").animate({
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000);
+})
+
+$("#btnGo").click(function() {
+    $("body,html").animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+})
+
+$("[data-original]").click(function() {
+    window.open($(this).data('original'), '_blank');
+})
+
+function getRandom() {
+    let min = +document.querySelector('#min').value;
+    let max = +document.querySelector('#max').value;
+    let rand = Math.floor(min + Math.random() * (max - min + 1));
+    document.querySelector('.random-result').innerText = rand;
+}
